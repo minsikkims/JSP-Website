0a75f0b274ef47bc43836dc26fd56be7f0a3b806
minsikkims <wozn159@nate.com>
2022-12-27 17:30:57 +0900

    2022/12/27

--- src/main/java/Dao/MemberDao.java
+++ src/main/java/Dao/MemberDao.java
@@ -42,71 +42,133 @@ public class MemberDao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+			}
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
 		}
 		return isok;
 
 	}
 
 	public int maxcustno() {
-		int result=0;
+		int result = 0;
 		try {
 			pstmt = con.prepareStatement("select max(custno) from member_tbl_02");
 			rs = pstmt.executeQuery();
-			if(rs !=null)
-			{
+			if (rs != null) {
 				rs.next();
 				result = rs.getInt(1);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {rs.close();} catch (Exception e) {}
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {
+				rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+			}
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
 		}
-		
+
 		return result;
 	}
 
-	
 	// UPDATE
-	public void Update(MemberDto dto) {
+	public boolean Update(MemberDto dto) {
+		boolean isok = false;
 		try {
-
+			pstmt = con.prepareStatement(
+					"update member_tbl_02 set custname=?,phone=?,address=?,joindate=?,grade=?,city=? where custno=?");
+			pstmt.setString(1, dto.getCustname());
+			pstmt.setString(2, dto.getPhone());
+			pstmt.setString(3, dto.getAddress());
+			pstmt.setString(4, dto.getJoindate());
+			pstmt.setString(5, dto.getGrade());
+			pstmt.setString(6, dto.getCity());
+			pstmt.setInt(7, Integer.parseInt(dto.getCustno()));
+			int result=pstmt.executeUpdate();
+			if(result>0)
+				isok=true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-
+			try {
+				rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+			}
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
 		}
+		return isok;
 	}
 
 	// SELECT
-	public void Select(MemberDto dto) {
+	public MemberDto Select(int custno) {
+		MemberDto dto = null;
 		try {
+			pstmt = con.prepareStatement("select * from member_tbl_02 where custno=?");
+			pstmt.setInt(1, custno);
+			rs = pstmt.executeQuery();
+			if (rs != null) {
+				rs.next();
+				dto = new MemberDto();
+				dto.setCustno(rs.getInt("custno") + "");
+				dto.setCustname(rs.getString("custname"));
+				dto.setPhone(rs.getString("phone"));
+				dto.setJoindate(rs.getString("joindate"));
+				dto.setAddress(rs.getString("address"));
+				dto.setGrade(rs.getString("grade"));
+				dto.setCity(rs.getString("city"));
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-
+			try {
+				rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+			}
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
 		}
+		return dto;
 	}
 
 	// SELECTALL
 	public List<MemberDto> SelectAll() {
 		List<MemberDto> list = new ArrayList();
-		MemberDto dto=null;
+		MemberDto dto = null;
 		try {
 			pstmt = con.prepareStatement("select * from member_tbl_02");
-			rs=pstmt.executeQuery();
-			if(rs!=null)
-			{
-				while(rs.next())
-				{
+			rs = pstmt.executeQuery();
+			if (rs != null) {
+				while (rs.next()) {
 					dto = new MemberDto();
-					dto.setCustno(rs.getInt("custno")+"");
+					dto.setCustno(rs.getInt("custno") + "");
 					dto.setCustname(rs.getString("custname"));
 					dto.setPhone(rs.getString("phone"));
 					dto.setAddress(rs.getString("address"));
@@ -115,15 +177,24 @@ public class MemberDao {
 					dto.setCity(rs.getString("city"));
 					list.add(dto);
 				}
-				
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {rs.close();} catch (Exception e) {}
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {
+				rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+			}
+			try {
+				con.close();
+			} catch (Exception e) {
+			}
 		}
 		return list;
 	}
